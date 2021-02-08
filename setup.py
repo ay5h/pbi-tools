ed24d698f249a086a34b1b67e6cdb5c13efbc8ad
Sam Thomas <sam.thomas@diageo.com>
2021-02-08 23:16:52 +0000

    Rename repo

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pbi-api-thomas-daughters',
+    name='pbi-tools',
     version='0.0.6',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Power BI REST API wrapper and other tools',
     long_description=open('README.md').read(),
     install_requires=['requests','gitpython'],
-    url='https://github.com/thomas-daughters/pbi-api',
+    url='https://github.com/thomas-daughters/pbi-tools',
     author='Sam Thomas',
     author_email='sam.thomas@redkite.com'
 )
