922298b324e053f729acf9ea2a7b92926bba3aac
Sam Thomas <sam.thomas@diageo.com>
2021-02-08 22:41:56 +0000

    Simplify build files

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup()
+setup(
+    name='pbi-api-thomas-daughters',
+    version='0.0.3',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Power BI REST API wrapper and other tools',
+    long_description=open('README.md').read(),
+    install_requires=['requests,gitpython'],
+    url='https://github.com/thomas-daughters/pbi-api',
+    author='Sam Thomas',
+    author_email='sam.thomas@redkite.com'
+)
