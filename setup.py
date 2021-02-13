56add2ab8e70a949354041be6df7c4e02520a6ce
Sam Thomas <sam.thomas@diageo.com>
2021-02-13 22:17:42 +0000

    Increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pbi-tools',
-    version='0.0.11',
+    version='0.0.12',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Power BI REST API wrapper and other tools',
