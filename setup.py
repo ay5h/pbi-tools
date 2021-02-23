3ce94b234b2a20006b8339f2d377acd2e5fe8c25
Sam Thomas <sam.thomas@diageo.com>
2021-02-23 21:16:16 +0000

    Expose name comparator function

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pbi-tools',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Power BI REST API wrapper and other tools',
