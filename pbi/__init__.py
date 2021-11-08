8d378881919ddcf76055e8e8322949ede2855398
Sam Thomas <thomas.sam@me.com>
2021-11-08 22:43:42 +0000

    Added dynamic versioning using git tags #major (#30)
    Co-authored-by: Sam Thomas <sam.thomas@redkite.com>

--- pbi/__init__.py
+++ pbi/__init__.py
@@ -6,3 +6,5 @@ from .report import Report
 from .tenant import Tenant
 from .token import Token
 from .workspace import Workspace
+from . import _version
+__version__ = _version.get_versions()['version']
