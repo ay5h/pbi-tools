01c1c00c921f226ead6ea4ef24a1f7e613ded082
Sam Thomas <sam.thomas@redkite.com>
2021-11-10 23:01:41 +0000

    Combine redkite-pbi-tools

--- src/pbi/__init__.py
+++ src/pbi/__init__.py
@@ -1,10 +1,10 @@
+from . import api
+
 from . import tools
-from .capacity import Capacity
-from .dataset import Dataset
-from .datasource import Datasource
-from .report import Report
+from . import deploy
 from .tenant import Tenant
 from .token import Token
-from .workspace import Workspace
+from .portal import Portal
+
 from . import _version
 __version__ = _version.get_versions()['version']
--- src/pbi/tools.py
+++ src/pbi/tools.py
@@ -1,4 +1,5 @@
 import os
+from git import Repo
 import zipfile as zf
 
 def handle_request(r, allowed_codes=None):
@@ -48,3 +49,24 @@ def get_connection_string(filepath):
         for f in zip_file.namelist():
             if f == 'Connections':
                 return zip_file.read(f)
+
+def check_file_modified(filepath):
+    """Checks whether the given file was modified in the most recent commit. Only works if the file is within a git repo.
+
+    :param filepath: path to the file being examined
+    :return: a boolean indicating whether the file was modified in the most recent commit
+    """
+
+    repo = Repo(filepath, search_parent_directories=True)
+    diff = repo.head.commit.diff('HEAD~1')
+    filepath_end = filepath.replace('\\', '/').split('pbi/')[-1]
+    
+    return any(f.b_path.split('pbi/')[-1] == filepath_end and f.change_type in ['A', 'M', 'R'] for f in diff)
+
+def open_branches():
+    """Returns a list of open git branches. Only works if the current working directory is within a git repo.
+    
+    :return: an array of branch names
+    """
+
+    return [b.remote_head for b in Repo(os.path.abspath(__file__), search_parent_directories=True).remotes.origin.refs]
