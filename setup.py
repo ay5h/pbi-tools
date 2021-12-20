e10d93eb997c0c3c2e1dbe3b444c5d3d0f7c8f9a
Sam Thomas <sam.thomas@redkite.com>
2021-12-20 14:29:08 +0000

    Change versioning schema

--- setup.py
+++ setup.py
@@ -7,7 +7,10 @@ setup(
     license="MIT",
     description="Power BI REST API wrapper and other tools",
     long_description=open("README.md").read(),
-    use_scm_version={"local_scheme": "no-local-version"},
+    use_scm_version={
+        "version_scheme": "release-branch-semver",
+        "local_scheme": "no-local-version"
+    },
     setup_requires=['setuptools_scm'],
     install_requires=["gitpython", "requests"],
     url="https://github.com/thomas-daughters/pbi-tools",
