f48bd032365345efea8b79725baa5c229fa1c0bb
Sam Thomas <sam.thomas@redkite.com>
2021-12-20 13:39:16 +0000

    Change runtime version  from versioneer to setuptools_scm

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup, find_packages
-import versioneer
+from importlib.metadata import version
 
 setup(
     name="pbi-tools",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+    version=version("package-name"),
+#    cmdclass=versioneer.get_cmdclass(),
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     license="MIT",
     description="Power BI REST API wrapper and other tools",
     long_description=open("README.md").read(),
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     install_requires=["gitpython", "requests"],
     url="https://github.com/thomas-daughters/pbi-tools",
     author="Sam Thomas",
